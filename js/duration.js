2587a8ec2943b83e106c4b71b0d5dc477a5797ec
康 吴 <1659757992@qq.com>
2023-08-08 13:42:12 +0800

    Site updated: 2023-08-08 13:42:10

--- js/duration.js
+++ js/duration.js
@@ -1,14 +1,31 @@
-var now = new Date();
-function createtime() {
-    var grt= new Date("03/03/2021 12:00:00");//�ڴ˴��޸���Ľ�վʱ�䣬��ʽ����/��/�� ʱ:��:��
+!(function() {
+  /** ��ʱ��ʼʱ�䣬�����޸� **/
+  var start = new Date("2023/08/08 00:00:00");
+
+  function update() {
+    var now = new Date();
     now.setTime(now.getTime()+250);
-    days = (now - grt ) / 1000 / 60 / 60 / 24; dnum = Math.floor(days);
-    hours = (now - grt ) / 1000 / 60 / 60 - (24 * dnum); hnum = Math.floor(hours);
-    if(String(hnum).length ==1 ){hnum = "0" + hnum;} minutes = (now - grt ) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
-    mnum = Math.floor(minutes); if(String(mnum).length ==1 ){mnum = "0" + mnum;}
-    seconds = (now - grt ) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
-    snum = Math.round(seconds); if(String(snum).length ==1 ){snum = "0" + snum;}
-    document.getElementById("timeDate").innerHTML = "��վ�Ѱ�ȫ���� "+dnum+" �� ";
-    document.getElementById("times").innerHTML = hnum + " Сʱ " + mnum + " �� " + snum + " ��";
-}
-setInterval("createtime()",250);
+    days = (now - start) / 1000 / 60 / 60 / 24;
+    dnum = Math.floor(days);
+    hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+    hnum = Math.floor(hours);
+    if(String(hnum).length === 1 ){
+      hnum = "0" + hnum;
+    }
+    minutes = (now - start) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+    mnum = Math.floor(minutes);
+    if(String(mnum).length === 1 ){
+      mnum = "0" + mnum;
+    }
+    seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+    snum = Math.round(seconds);
+    if(String(snum).length === 1 ){
+      snum = "0" + snum;
+    }
+    document.getElementById("timeDate").innerHTML = "��վ��ȫ����&nbsp"+dnum+"&nbsp��";
+    document.getElementById("times").innerHTML = hnum + "&nbspСʱ&nbsp" + mnum + "&nbsp��&nbsp" + snum + "&nbsp��";
+  }
+
+  update();
+  setInterval(update, 1000);
+})();
